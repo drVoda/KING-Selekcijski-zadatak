1604a07afe7931e7c31d85d897c9aa550b7f2cee
drVoda <vodopija16@gmail.com>
2024-06-17 22:25:48 +0200

    bootstrapped project

---
+++ src/main/java/hr/king/selectionProject/SelectionProjectApplication.java
@@ -0,0 +1,13 @@
+package hr.king.selectionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/king/selectionProject/SelectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.king.selectionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
